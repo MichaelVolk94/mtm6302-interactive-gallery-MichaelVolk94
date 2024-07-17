ac35a053be79a76e08f8c98f7327baa4c62eaa24
Michael Volk <volk0026@algonquinlive.com>
2024-07-17 14:36:13 -0400

    Completes the assignment

--- js/script.js
+++ js/script.js
@@ -2,9 +2,16 @@ const items = ['Boats', 'Kamnik', 'Candles', 'Portrait', 'Leaves', 'Background',
 const $pictures = document.getElementById('pictures');
 const html = items.map(item => `
     <p>
-        <img src="images/${item}.png">
+        <img src="images/${item}.png" class = "image-button" data-image = ${item}>
         <code>${item}</code>
     </p>
 `).join('');
-
 $pictures.innerHTML = html;
+$pictures.addEventListener('click', function(event){
+if (event.target.classList.contains('image-button')){
+    document.getElementById("HD-image").innerHTML = `<p>
+    <img src="images/${event.target.dataset.image}.png">
+    <code>${event.target.dataset.image}</code>
+</p>`
+}
+})
